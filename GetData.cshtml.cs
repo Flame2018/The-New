3400f251d1d2a3debe09846e5f34236b16f4fe88
Flame2018 <54338887+Flame2018@users.noreply.github.com>
2019-10-24 14:58:53 -0500

    Add files via upload

---
+++ GetData.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ShinobiRanks1._0.Data;
+
+namespace ShinobiRanks1._0.Pages
+{
+    public class GetDataModel : PageModel
+    {
+        public ApplicationDbContext _DB { get; set; }
+
+        public IList<HVGenin> HVGenin { get; set; }
+
+        public GetDataModel(ApplicationDbContext DB)
+        {
+            _DB = DB;
+        }
+
+        public async Task OnGetAsync()
+        {
+            HVGenin = await _DB.HVGenin.ToListAsync();
+        }
+    }
+}
